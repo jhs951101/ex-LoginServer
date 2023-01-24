b8b07939b539bc0ccc32a5ba4b56c433c2df5df4
jhs951101 <spg1101@naver.com>
2023-01-24 15:34:22 -0800

    Added sql file

--- getled.php
+++ getled.php
@@ -4,18 +4,23 @@
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Credentials: true');
 
+    $response = array();
+    $response['success'] = false;
+
     $mysqlController = new MysqlController();
 
     $query = 'SELECT * FROM test_led LIMIT 1';
     $results = $mysqlController->executeQuery($query);
 
     if($results['success']){
-		if(count($results['results']) >= 1){
-			$response['name'] = $results['results'][0]['name'];
-		}
-        else {
-            $response['name'] = 'red';
+        if(count($results['results']) >= 1){
+            $response['name'] = $results['results'][0]['name'];
         }
+        else {
+            $response['name'] = 'NULL';
+        }
+
+        $response['success'] = true;
     }
 
     echo json_encode($response, JSON_UNESCAPED_UNICODE);
